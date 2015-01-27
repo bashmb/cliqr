afa9e0f21efcdae006f0e4e2f39a5b1235485b83
Glenn Meyer <glennmeyer@gmail.com>
2015-01-27 14:10:26 -0600

    association tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :questions 
   has_many :answers
+  has_many :downvotes
+  has_many :upvotes
 
   validates :email, presence: true
   validates :password, presence: true
