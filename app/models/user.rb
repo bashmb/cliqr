3355a2b7cced2335b5e9eaf36d255e21f6bc180b
charles milam <charles.milam@gmail.com>
2015-01-28 12:30:54 -0600

    Modify users model - cleanup of code, placed uniqueness in proper location. Modify user text fixture - comment out default load of fixture data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,29 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable #, :validatable
-  validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }
-                    # uniqueness: {case_sensitive: false}       
+  
   has_many :questions 
   has_many :answers
-  #before_save { self.email = email.downcase }
-  # validates :email, presence: true, length: { maximum: 255 },
-  #                   format: { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
-  # # has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*[0-9]).{6,}/ }
+  # has_secure_password
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :validatable
+
+  validates :email, 
+            presence: true, 
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 255}, 
+            format: { with: VALID_EMAIL_REGEX }
+            
+  validates :password, 
+            presence: true, 
+            length: { minimum: 6 }, 
+            format: { with: /(?=.*[a-zA-Z])(?=.*[0-9]).{6,}/ }
+     
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  
 end
