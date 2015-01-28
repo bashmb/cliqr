b9118c9bcb3ed2f84d29dcf4e35816d5b5a410a5
charles milam <charles.milam@gmail.com>
2015-01-28 15:24:23 -0600

    WIP: Begin test of saving of a question's up vote?

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  test "content type should be question or answer" do
-    
+  def setup
+    @user = User.new
+    @user.email = "testerino@test.com"
+    @user.password = "abcd1234"
+    @user.presenter = false
+    @user.save
+  end
+
+  def teardown
+    @user = nil
+  end
+
+  test "should create a question up vote" do
+
+    @user.questions.create text: "This is a test question?"
+    @user.questions.votes.create @user.questions.first
   end
 end
