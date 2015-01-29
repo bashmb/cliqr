91b11400b4fdb63e9e484401a088bca22269f5d4
Charles <charles.milam@gmail.com>
2015-01-29 00:29:10 -0600

    Add test for check of nil value in user_id

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -28,4 +28,33 @@ class VoteTest < ActiveSupport::TestCase
 
     assert_equal "Question", ctype, "Content type is not 'Question'"
   end
+
+  test "should create a question upvote" do
+    @vote = @question.votes.new 
+    @vote.content_type = "Question"
+    @vote.vote_type = "upvote"
+    @vote.save
+    vtype = @vote.vote_type
+
+    assert_equal "upvote", vtype, "Vote type is not 'upvote'"
+  end
+
+  test "should create a question downvote" do
+    @vote = @question.votes.new 
+    @vote.content_type = "Question"
+    @vote.vote_type = "downvote"
+    @vote.save
+    vtype = @vote.vote_type
+
+    assert_equal "downvote", vtype, "Vote type is not 'downvote'"
+  end
+
+  test "user_id should not be nil" do
+    @vote = @question.votes.new 
+    @vote.content_type = "Question"
+    @vote.vote_type = "upvote"
+    @vote.save
+
+    assert_not_nil @vote.user_id, "Saved with user_id nil"
+  end
 end
