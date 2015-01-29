b828962106fad5739c0e606d3e925ec2a8aa328f
Charles <charles.milam@gmail.com>
2015-01-29 13:55:01 -0600

    Add test for nil/empty value in answer text. Modify - cleanup up of remnants dblpoly in models.

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,23 +7,4 @@ class Question < ActiveRecord::Base
   validates :user_id, presence:  true
   validates :text, presence: true
 
-  # you may not need the lines below
-  #
-  # def upvotes
-  #   opts = {
-  #     content_id: self.id,
-  #     content_type: "Question",
-  #     vote_type: "Upvote"
-  #   }
-  #   Vote.where opts
-  # end
-
-  # def downvotes
-  #   opts = {
-  #     content_id: self.id,
-  #     content_type: "Question",
-  #     vote_type: "Downvote"
-  #   }
-  #   Vote.where opts
-  # end
 end
