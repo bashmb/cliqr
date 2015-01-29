2950fe63a16457892247bef224bf5a34009a8486
Charles <charles.milam@gmail.com>
2015-01-29 00:09:26 -0600

    Add test for creation of content type "question"

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -8,28 +8,24 @@ class VoteTest < ActiveSupport::TestCase
     @user.password = "abcd1234"
     @user.presenter = false
     @user.save
+
+    # a test question
+    @question = @user.questions.new
+    @question.text = "This is a test question?"
+    @question.save
   end
 
   teardown do
     @user = nil
   end
 
-  test "should create a question up vote" do
-    puts "user ", @user
-    q = @user.questions.new
-    q.text = "This is a test question?"
-    q.save
-    puts "question", q.id, q.text
-    # @user.questions.new(text: "This is a test question?").save
-    # puts "user questions", @user.questions.find_by(id: 1).text
-    @user.questions.first.votes.create content_type: "Question", vote_type: "upvote"
-    
-    puts "user votes", @user.questions.count, 
-        @user.questions.first.votes.first.content_type,
-        @user.questions.first.votes.first.vote_type,
-        @user.questions.first.votes.first.id,
-        @user.questions.first.votes.first.user_id
+  test "should create a question" do
+    @vote = @question.votes.new 
+    @vote.content_type = "Question"
+    @vote.vote_type = "upvote"
+    @vote.save
+    ctype = @vote.content_type
 
-
+    assert_equal "Question", ctype, "Content type is not 'Question'"
   end
 end
