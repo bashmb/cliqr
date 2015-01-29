d7adb53d9319a8d3dc51cd26157d7e888092f802
Charles <charles.milam@gmail.com>
2015-01-28 23:33:12 -0600

    Add basic test functionality

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -8,9 +8,10 @@ class VoteTest < ActiveSupport::TestCase
     @user.password = "abcd1234"
     @user.presenter = false
     @user.save
+  end
 
-    # a test question
-    #@user.questions.create text: "This is a test qustion?"
+  teardown do
+    @user = nil
   end
 
   test "should create a question up vote" do
@@ -21,8 +22,14 @@ class VoteTest < ActiveSupport::TestCase
     puts "question", q.id, q.text
     # @user.questions.new(text: "This is a test question?").save
     # puts "user questions", @user.questions.find_by(id: 1).text
-    @user.questions.find_by(id: 1).votes.create content_type: "Question", vote_type: "upvote"
+    @user.questions.first.votes.create content_type: "Question", vote_type: "upvote"
     
-    puts "user votes", @user.questions
+    puts "user votes", @user.questions.count, 
+        @user.questions.first.votes.first.content_type,
+        @user.questions.first.votes.first.vote_type,
+        @user.questions.first.votes.first.id,
+        @user.questions.first.votes.first.user_id
+
+
   end
 end
