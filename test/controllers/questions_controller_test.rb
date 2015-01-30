2f1e535ef5e286e713a79b5ef58397db205fd8bb
Charles <charles.milam@gmail.com>
2015-01-30 10:07:59 -0600

    Add necessary includes to test controllers for use with devise. Fixes issue #77

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
+  # needed for devise and warden
+  include Devise::TestHelpers
+  include Warden::Test::Helpers
+  # start warden test mode
+  Warden.test_mode!
+
+  setup do
+    # needed setup code before each test would go here
+  end
+
+  teardown do
+    # reset the Warden test mode after each test
+    Warden.test_reset!
+  end
+
   test "should get create" do
     get :create
     assert_response :success
