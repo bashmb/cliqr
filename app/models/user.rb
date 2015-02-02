c39a9e201cdaca84cb9416fe5b8d2dd9473fc315
Glenn Meyer <glennmeyer@gmail.com>
2015-02-02 10:47:38 -0600

    resolved issue 72

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,18 +12,18 @@ class User < ActiveRecord::Base
          :validatable
 
   validates :email, 
-            presence: true, 
-            uniqueness: {case_sensitive: false},
-            length: {maximum: 255}, 
-            format: { with: VALID_EMAIL_REGEX }
+            presence: {message: "cannot be blank"}, 
+            uniqueness: {case_sensitive: false, message: "already in use"},
+            length: {maximum: 255, message: "must be less than 255 characters"}, 
+            format: { with: VALID_EMAIL_REGEX, message: "requires valid alphanumerics" }
             
   validates :password, 
-            presence: true, 
-            format: { with: /(?=.*[a-zA-Z])(?=.*[0-9]).{6,}/ }
+            presence: {message: "cannot be blank"}, 
+            format: { with: /(?=.*[a-zA-Z])(?=.*[0-9]).{6,}/, message: "must contain letters and numbers" }
      
   before_save { self.email = email.downcase }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                  format: { with: VALID_EMAIL_REGEX },
+  #                  uniqueness: { case_sensitive: false }
   
 end
