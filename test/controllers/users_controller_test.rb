caf6670164f37da70db923b7e671dda4dc72f633
Charles <charles.milam@gmail.com>
2015-02-03 14:23:41 -0600

    Modify tests for proper assertions.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,31 +31,30 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path, "Did not redirect to new user session sign-in"
   end
   
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+  test "should get create" do
+    post :create, user: {email: "test123@test.com", password: "abcd1234", presenter: false}
+    assert_redirected_to root_path
+  end
 
   test "should get destroy and redirect to " do
     sign_in @user
 
     delete :destroy, id: @user.id
 
-    #assert_response :redirect, "Did not succeed"
     assert_redirected_to root_path, "Did not redirect to the root path"
   end
 
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    sign_in @user
+
+    get :edit, id: @user.id
+
+    assert_response :success
+  end
 
   test "Authenicated user should get index" do
     sign_in users(:user1)
-    # puts "in auth index test", @user.id, @user.presenter
-    # puts "auth user index signed in", sign_in(@user)
-    # sign_in :user, @user
-    # puts "in auth index test", @user.id, @user.presenter
+
     get :index
 
     assert_response :success, "Authenticated user did not get index"
